123b0218d7dddb29842d2af83826d9a05ac1191b
DSavrasov <a@a>
2024-06-03 02:49:49 +0300

    Задание N 2. Рефлексия. Исправление замечаний

--- src/main/java/ru/stepup/Account.java
+++ src/main/java/ru/stepup/Account.java
@@ -16,7 +16,7 @@ public class Account {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть null или пустым");
         }
-        saveToHistory();
+        saveToHistory(this.name, null, 0); //изменилось только имя, валюту не будем сохранять в историю
         this.name = name;
     }
 
@@ -43,47 +43,63 @@ public class Account {
             throw new IllegalArgumentException("Количество не может быть отрицательным");
         }
         //если валюта есть в списке, то обновляем количество, если нет, добавляем
-        saveToHistory(); //сохраняем предыдущее состояние
+
+        if (values.containsKey(currency))
+            saveToHistory(null,currency,values.get(currency)); //сохраняем предыдущее состояние
+        else
+            saveToHistory(null,currency,0);
         values.put(currency, amount);
     }
 
-    private void saveToHistory() {
-        history.push(new AccountHistory(name, new HashMap<>(values)));
+    private void saveToHistory(String name, Currency currency, int amount) {
+        history.push(new AccountHistory(name, currency, amount));
     }
 
     public void undo() {
         if (!history.isEmpty()) {
             AccountHistory hist = history.pop();
-            this.name = hist.getName();
-            this.values = new HashMap<>(hist.getValues());
+            if (hist.getName()!=null) //если имя изменилось, то восстанавливаем предыдущее состояние
+                name = hist.getName();
+            if (hist.getCurrency()!=null && hist.getAmount()!=0) //если имя не изменилось, то восстанавливаем количество валюты
+                values.put(hist.getCurrency(), hist.getAmount());
+            if (hist.getCurrency()!=null && hist.getAmount()==0) //если имя не изменилось, то удаляем валюту
+                values.remove(hist.getCurrency());
         }
     }
 
     private static class AccountHistory {
         private final String name;
-        private final Map<Currency, Integer> values;
+        private final Currency currency;
+        private final int amount;
 
-        public AccountHistory(String name, Map<Currency, Integer> values) {
+        public AccountHistory(String name, Currency currency, int amount) {
             this.name = name;
-            this.values = values;
+            this.currency = currency;
+            this.amount = amount;
         }
+
 
         public String getName() {
             return name;
         }
 
-        public Map<Currency, Integer> getValues() {
-            return values;
+        public Currency getCurrency() {
+            return currency;
+        }
+
+        public int getAmount() {
+            return amount;
         }
     }
 
     //3
     public AccountSnapshot saveToSnapshot() {
-        return new AccountSnapshot(this.name, new HashMap<>(this.values));
+        return AccountSnapshot.createInstance(this);
     }
 
     public void restoreFromSnapshot(AccountSnapshot snapshot) {
         this.name = snapshot.getName();
         this.values = new HashMap<>(snapshot.getValues());
     }
+
 }
