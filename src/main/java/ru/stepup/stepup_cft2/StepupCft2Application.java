f5842e7a5c16dd45c77827f79240ceb0d6125556
DenisS <d_savrasov@rambler.ru>
2024-06-17 22:11:45 +0300

    1 commit

---
+++ src/main/java/ru/stepup/stepup_cft2/StepupCft2Application.java
@@ -0,0 +1,13 @@
+package ru.stepup.stepup_cft2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepupCft2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepupCft2Application.class, args);
+	}
+
+}
