0f26c6bcc5c70702951402ad0b09d58fefb80b3c
DenisS <d_savrasov@rambler.ru>
2024-06-24 23:17:55 +0300

    6 commit

--- src/main/java/ru/stepup/AccessDateChecker.java
+++ src/main/java/ru/stepup/AccessDateChecker.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,13 +14,14 @@ public class AccessDateChecker implements Checker<Login>{
     private static final Logger logger = LoggerFactory.getLogger(AccessDateChecker.class);
 
     public List<Login> check(List<Login> logins) {
-        return logins.stream()
-                .peek(login -> {
-                    if (login.getAccessDate() == null) {
-                        logger.error("Поле access_date пустое " + login.getId());
-                    }
-                })
-                .filter(login -> login.getAccessDate() != null)
-                .collect(Collectors.toList());
+        List<Login> result = new ArrayList<>();
+        for (Login login : logins) {
+            if (login.getAccessDate() == null) {
+                logger.error("Поле access_date пустое " + login.getId());
+            } else {
+                result.add(login);
+            }
+        }
+        return result;
     }
 }
